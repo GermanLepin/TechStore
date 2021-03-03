6961a08269839479963abdc16653faf123dbbf55
german <g.lepin@mail.ru>
2021-03-03 12:26:22 +0300

    fourth commit

--- shop/urls.py
+++ shop/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
     path('cart', views.cart_detail, name='cart_detail'),
     path('cart/add/<int:product_id>', views.add_cart, name = 'add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name = 'remove_cart'),
+    path('cart/remove_product/<int:product_id>', views.cart_remove_product, name = 'remove_cart_product'),
 ]
+
+
+
